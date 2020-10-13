81be135edde499f8d780118438d07f0ab72c835c
Julian <julian.ollivieira@gmail.com>
2020-10-13 11:04:48 +0200

    Update screenshot

--- src/PlayerStore.js
+++ src/PlayerStore.js
@@ -1,9 +1,9 @@
 import { writable } from 'svelte/store';
 
 const PlayerStore = writable([
-    { name: 'John Doe',     points: 50,     uuid: 1 },
-    { name: 'John Doe 2',   points: 100,    uuid: 2 },
-    { name: 'John Doe 3',   points: 150,    uuid: 3 },
+    { name: 'John Doe',     points: 50,     uuid: 'auq81hdcmcro17wxdgsj2p' },
+    { name: 'John Doe 2',   points: 100,    uuid: 'mv8wf1sehgc1im4175igdx' },
+    { name: 'John Doe 3',   points: 150,    uuid: 'uglzmvcnxbost046autdj'  },
 ]);
 
 export default PlayerStore;
